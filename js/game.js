1c8d482b8d3300da17cd9348226aca39b6100936
jongho <jh01010@doctorsoft.co.kr>
2023-09-08 17:59:30 +0900

    수리검

--- js/game.js
+++ js/game.js
@@ -1,50 +1,59 @@
 const key = {
-    keyDown: {},
-    keyValue: {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down',
-        88: 'attack',
-    }
-}
+  keyDown: {},
+  keyValue: {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+    88: 'attack',
+  },
+};
+
+const bulletComProp = {
+  launch: false,
+  arr: [],
+};
 
 const gameProp = {
-    screenWidth: window.innerWidth,
-    screenHeight: window.innerHeight,
-}
+  screenWidth: window.innerWidth,
+  screenHeight: window.innerHeight,
+};
 
 const renderGame = () => {
-    hero.keyMotion();
-    window.requestAnimationFrame(renderGame);
-}
+  hero.keyMotion();
+
+  bulletComProp.arr.forEach((v, i) => {
+    v.moveBullet();
+  });
+  window.requestAnimationFrame(renderGame);
+};
 
 const windowEvent = () => {
-    window.addEventListener('keydown', e => {
-        key.keyDown[key.keyValue[e.which]] = true;
-    });
+  window.addEventListener('keydown', (e) => {
+    key.keyDown[key.keyValue[e.which]] = true;
+  });
 
-    window.addEventListener('keyup', e => {
-        key.keyDown[key.keyValue[e.which]] = false;
-    });
+  window.addEventListener('keyup', (e) => {
+    key.keyDown[key.keyValue[e.which]] = false;
+  });
 };
 
 const loadImg = () => {
-    const preLoadImgSrc = ['../lib/images/ninja_attack.png', '../lib/images/ninja_run.png'];
-    preLoadImgSrc.forEach(v => {
-        const img = new Image();
-        img.src = v;
-    });
-}
+  const preLoadImgSrc = ['../lib/images/ninja_attack.png', '../lib/images/ninja_run.png'];
+  preLoadImgSrc.forEach((v) => {
+    const img = new Image();
+    img.src = v;
+  });
+};
 
 let hero;
 const init = () => {
-    hero = new Class('.hero');
-    loadImg();
-    windowEvent();
-    renderGame();
+  hero = new Hero('.hero');
+  loadImg();
+  windowEvent();
+  renderGame();
 };
 
 window.onload = () => {
-    init();
+  init();
 };
