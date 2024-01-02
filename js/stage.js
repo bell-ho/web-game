1e528a4e4326dbaead07ca884219b8843528ed12
jongho <jh01010@doctorsoft.co.kr>
2024-01-02 13:57:01 +0900

    npc 생성

--- js/stage.js
+++ js/stage.js
@@ -1,17 +1,7 @@
 class Stage {
   constructor() {
     this.level = 0;
-    this.isStart = false;
-    // this.stageStart();
   }
-
-  // stageStart() {
-  //   setTimeout(() => {
-  //     this.isStart = true;
-  //     this.stageGuide(`START LEVEL${this.level + 1}`);
-  //     this.callMonster();
-  //   }, 2000);
-  // }
 
   stageGuide(stageLevel) {
     this.parentNode = document.querySelector('.game_app');
@@ -25,20 +15,12 @@ class Stage {
   }
 
   callMonster() {
-    Array(10)
+    allMonsterComProp.arr = Array(10)
       .fill()
-      .forEach((_, i) => {
-        if (i === 9) {
-          allMonsterComProp.arr[i] = new Monster(
-            stageInfo.monster[this.level].bossMon,
-            hero.moveX + gameProp.screenWidth + 600 * i,
-          );
-        } else {
-          allMonsterComProp.arr[i] = new Monster(
-            stageInfo.monster[this.level].defaultMon,
-            hero.moveX + gameProp.screenWidth + 700 * i,
-          );
-        }
+      .map((_, i) => {
+        const monsterType = i === 9 ? stageInfo.monster[this.level].bossMon : stageInfo.monster[this.level].defaultMon;
+        const position = hero.moveX + gameProp.screenWidth + (i === 9 ? 600 : 700) * i;
+        return new Monster(monsterType, position);
       });
   }
 
@@ -54,17 +36,5 @@ class Stage {
         }, 1000);
       }
     });
-    // if (allMonsterComProp.arr.length === 0 && this.isStart) {
-    //   this.isStart = false;
-    //   this.level++;
-    //
-    //   if (this.level < stageInfo.monster.length) {
-    //     this.stageGuide('CLEAR!!');
-    //     this.stageStart();
-    //     hero.heroUpgrade();
-    //   } else {
-    //     this.stageGuide('ALL CLEAR!!');
-    //   }
-    // }
   }
 }
